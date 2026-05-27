6226aa64dbdfd711f063e96bafadf8a7207f8aa1
palharsh470 <palharsh470@gmail.com>
2026-05-28 01:20:46 +0530

    Fix import path for ChatBox CSS file

--- frontend/src/components/ChatBox.jsx
+++ frontend/src/components/ChatBox.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 
-import "../components/Chatbox.css"
+import "../components/ChatBox.css"
 export default function ChatBox({ socket, roomId, playerId, messages, isDrawing, isGuessed }) {
   const [input, setInput] = useState("");
 
