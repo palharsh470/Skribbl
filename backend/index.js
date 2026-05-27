c7e72eb0fb975f1db668f10e0bf3eef708af6203
palharsh470 <palharsh470@gmail.com>
2026-05-28 01:31:10 +0530

    Add production URL to allowed origins for CORS

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,9 @@ const allowedOrigins = [
     "http://localhost:3001",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
+    "skribbl-4kga-m9p16aenj-harshs-projects-5c916aad.vercel.app"
+
+
 ];
 
 const io = new Server(server, {
